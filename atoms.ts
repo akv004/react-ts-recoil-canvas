251a72b113de395a22b725289c3083d773624917
akv004 <akv004@gmail.com>
2023-01-06 09:38:26 -0600

    1 file modified

--- atoms.ts
+++ atoms.ts
@@ -1,16 +1,16 @@
 import { atom } from 'recoil';
 
-export const textInput1State = atom({
-  key: 'textInput1',
-  default: '',
-});
+// export const textInput1State = atom({
+//   key: 'textInput1',
+//   default: '',
+// });
 
-export const textInput2State = atom({
-  key: 'textInput2',
-  default: '',
-});
+// export const textInput2State = atom({
+//   key: 'textInput2',
+//   default: '',
+// });
 
 export const textInputState = atom({
   key: 'textInput',
-  default: '',
+  default: '222',
 });
